dc6efc8845fb54aa020ddeaf30e01f81ab5855a3
FullStackJinnnn <ddoljin@gmail.com>
2024-01-03 23:42:50 +0900

    Fintech-day27

--- codeup/src/codeup/Main.java
+++ codeup/src/codeup/Main.java
@@ -1,29 +1,29 @@
-
 package codeup;
-
 import java.util.Scanner;
 
-public class Main {
-	public static void main(String[] args) {
+public class Main{
 
-		Scanner sc = new Scanner(System.in);
-		int i =2;
-		int answer =0;
-		while ( i > 0) {
-			int a = sc.nextInt();
-			int cnt = 0;
-			if (a % 2 != 0) {
-				while (a > 0) {
-					a -= 2;
-					cnt++;
-				}
-				a = cnt;
-			} else {
-				a = a / 2 * 10;
-			}
-			answer += a;
-			i--;
-		}
-		System.out.println(answer);
-	}
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+        String str;
+        int num2;
+     
+        System.out.print("num 입력> ");
+        num = scan.nextInt();
+        if (scan.hasNextLine()) {
+        	System.out.println("왕");
+        }
+        System.out.print("str 입력> ");
+        num2 = scan.nextInt();
+        //문제발생 : 개행문자(엔터)가  str에 저장됨
+        str = scan.nextLine();
+
+        System.out.println();
+        System.out.println("num : " + num);
+        System.out.println("str : " + str);
+        scan.close();
+    }
 }
